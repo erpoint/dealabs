bd57f0e97f5d4cf216680481081344597c20d6d1
Erina Point <erina.point@etu.uca.fr>
2023-06-22 20:56:29 +0200

    like system

--- src/Controller/DealDetailController.php
+++ src/Controller/DealDetailController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Comment;
 use App\Entity\Deal;
 use App\Form\AddCommentType;
+use App\Form\LikeType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,9 +22,9 @@ class DealDetailController extends AbstractController
         $comment = new Comment();
         $addCommentForm = $this->createForm(AddCommentType::class, $comment);
         $addCommentForm->handleRequest($request);
+        $likeForm = $this->createForm(LikeType::class, $deal);
+        $likeForm->handleRequest($request);
         if($addCommentForm->isSubmitted() && $addCommentForm->isValid()){
-            //dd($request->request);
-
             $comment = (new Comment())
                 ->setTheuser($this->getUser())
                 ->setDeal($deal)
@@ -31,10 +32,19 @@ class DealDetailController extends AbstractController
                 ->setCreatedAt(new \DateTimeImmutable());
             $em->getRepository(Comment::class)->save($comment, true);
         }
+        if($likeForm->isSubmitted() && $likeForm->isSubmitted()){
+            if($likeForm->get('degres_up')->isClicked()){
+                $deal->setDegres($deal->getDegres()+1);
+            }elseif($likeForm->get('degres_down')->isClicked()) {
+                $deal->setDegres($deal->getDegres()-1);
+            }
+            $em->getRepository(Deal::class)->save($deal, true);
+        }
         return $this->render('deal_detail/deal_detail.html.twig', [
             'controller_name' => 'DealDetailController',
             'deal' => $deal,
-            'addCommForm'=>$addCommentForm
+            'addCommForm'=>$addCommentForm,
+            'likeForm'=>$likeForm,
         ]);
     }
 }
